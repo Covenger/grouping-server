161e826e3e82d7078d5cf7038a0d77585f0d95fe
jeonsanghyun <jeonsang@linecorp.com>
2018-08-15 16:13:26 +0900

    project init

---
+++ src/main/java/com/covenger/grouping/GroupingApplication.java
@@ -0,0 +1,12 @@
+package com.covenger.grouping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupingApplication.class, args);
+    }
+}
