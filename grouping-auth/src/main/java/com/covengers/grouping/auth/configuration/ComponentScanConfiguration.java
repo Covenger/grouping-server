990e2369a05bec1ed0742c2d5ddbbeeeece4ce29
jeonsanghyun <jeonsang@linecorp.com>
2019-05-12 15:26:19 +0900

    add basic configuration frame

---
+++ grouping-auth/src/main/java/com/covengers/grouping/auth/configuration/ComponentScanConfiguration.java
@@ -0,0 +1,9 @@
+package com.covengers.grouping.auth.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+
+@Configuration
+@Order(1)
+public class ComponentScanConfiguration {
+}
