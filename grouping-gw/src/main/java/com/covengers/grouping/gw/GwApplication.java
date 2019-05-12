646c61784f9ce3e4dae7b45bb59bfc8004c49ac9
jeonsanghyun <jeonsang@linecorp.com>
2019-05-12 15:44:52 +0900

    Add empty controller for test to grouping-gw

--- grouping-gw/src/main/java/com/covengers/grouping/gw/GwApplication.java
+++ grouping-gw/src/main/java/com/covengers/grouping/gw/GwApplication.java
@@ -1,11 +1,14 @@
 package com.covengers.grouping.gw;
 
 import com.covengers.grouping.GroupingApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GwApplication extends GroupingApplication {
     public static void main(String[] args) {
-
+        SpringApplication app = new SpringApplication(GwApplication.class);
+        app.addListeners(new GroupingApplicationListener());
+        app.run(args);
     }
 }
---
+++ grouping-gw/src/main/java/com/covengers/grouping/gw/controller/AuthController.java
@@ -0,0 +1,9 @@
+package com.covengers.grouping.gw.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController("/v1/auth")
+public class AuthController {
+}
