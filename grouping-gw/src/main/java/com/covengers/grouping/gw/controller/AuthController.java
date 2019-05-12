1e11c89261c623f2274593eeab7cdcc87553ae4a
jeonsanghyun <jeonsang@linecorp.com>
2019-05-12 15:46:53 +0900

    Add internal annotation to AuthController for code readability

--- grouping-gw/src/main/java/com/covengers/grouping/gw/controller/AuthController.java
+++ grouping-gw/src/main/java/com/covengers/grouping/gw/controller/AuthController.java
@@ -1,9 +1,11 @@
 package com.covengers.grouping.gw.controller;
 
+import com.covengers.grouping.annotation.InternalApi;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
+@InternalApi
 @RestController("/v1/auth")
 public class AuthController {
 }
